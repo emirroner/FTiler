4d933848a859dfd1509a6712b6a5d84191d0aabf
press F <emirroner59@gmail.com>
2020-12-22 15:23:43 +0300

    Update FTiler.cpp

--- src/FTiler.cpp
+++ src/FTiler.cpp
@@ -148,7 +148,47 @@ std::string FTiler::mergeTileMap(std::vector<FTile> str)
 	return result;
 }
 
-std::string &FTiler::ltrim(std::string &s) {s.erase(s.begin(), std::find_if(s.begin(), s.end(),std::not1(std::ptr_fun<int, int>(std::isspace))));return s;}
-std::string &FTiler::rtrim(std::string &s) {s.erase(std::find_if(s.rbegin(), s.rend(),std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());return s;}
-std::string &FTiler::trim(std::string &s) {return ltrim(rtrim(s));}
-std::vector<std::string> FTiler::split(std::string str,std::string token){std::vector<std::string>result;std::string firststr = str;while(str.size()){size_t index = str.find(token);if(index!=std::string::npos){result.push_back(str.substr(0,index));str = str.substr(index+token.size());if(str.size()==0)result.push_back(str);} else {result.push_back(str);str = "";}}str = firststr[firststr.length()-1];if (str == token)result.pop_back();return result;}
+std::string &FTiler::ltrim(std::string &s)
+{
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(),std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+std::string &FTiler::rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(),std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+std::string &FTiler::trim(std::string &s) {
+	return ltrim(rtrim(s));
+}
+
+std::vector<std::string> FTiler::split(std::string str,std::string token)
+{
+	std::vector<std::string>result;
+	std::string firststr = str;
+
+	while(str.size())
+	{
+		size_t index = str.find(token);
+
+		if(index != std::string::npos)
+		{
+			result.push_back(str.substr(0,index));
+			str = str.substr(index+token.size());
+
+			if (str.size() == 0)
+				result.push_back(str);
+		} else {
+			result.push_back(str);
+			str = "";
+		}
+	}
+
+	str = firststr[firststr.length()-1];
+
+	if (str == token)
+		result.pop_back();
+
+	return result;
+}
